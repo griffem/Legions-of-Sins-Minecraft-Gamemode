2971459a5e94ca297e944e4f51f694ee94c8d67f
Torn <tiarlninbox@gmail.com>
2014-04-11 16:25:09 -0500

    Blindness added

--- Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/mechanics/deathworld/Bilndness.java
+++ Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/mechanics/deathworld/Bilndness.java
@@ -1,6 +1,10 @@
 package me.waternode.server.legion.of.sins.mechanics.deathworld;
 
+import me.waternode.server.legion.of.sins.InfinitePotionEffect;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * Created by Ervin
@@ -8,6 +12,11 @@ import org.bukkit.event.Listener;
  */
 public class Bilndness implements Listener {
 
-    
-
+    //better way to tell if player is in a world?
+    public void onPJE(PlayerJoinEvent event) {
+        if(event.getPlayer().getWorld().getName().contains("deathworld")) {
+            Player p = (Player) event.getPlayer();
+            p.addPotionEffect(new InfinitePotionEffect(PotionEffectType.BLINDNESS, 1));
+        }
+    }
 }
