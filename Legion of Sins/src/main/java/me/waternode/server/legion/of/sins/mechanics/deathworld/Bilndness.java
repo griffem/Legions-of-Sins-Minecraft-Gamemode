e801cb913eec0717141744d9bcdeafd9aa84fdc8
Torn <tiarlninbox@gmail.com>
2014-04-11 15:22:04 -0500

    Started work on Constant Blindness in the DeathWorld

---
+++ Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/mechanics/deathworld/Bilndness.java
@@ -0,0 +1,13 @@
+package me.waternode.server.legion.of.sins.mechanics.deathworld;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Created by Ervin
+ * on 4/11/2014
+ */
+public class Bilndness implements Listener {
+
+    
+
+}
