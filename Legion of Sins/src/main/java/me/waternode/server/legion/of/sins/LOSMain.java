b7a934619007959ac21b1dfb3ad4c7b9dc79cf49
Torn <tiarlninbox@gmail.com>
2014-04-11 22:50:39 -0500

    Simplifed DeathWorld Mob Handler

--- Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/LOSMain.java
+++ Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/LOSMain.java
@@ -14,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Random;
 
 public class LOSMain extends JavaPlugin implements Listener {
+
 	@Getter
 	private static LOSMain instance;
 	private WorldListener worldListener;
