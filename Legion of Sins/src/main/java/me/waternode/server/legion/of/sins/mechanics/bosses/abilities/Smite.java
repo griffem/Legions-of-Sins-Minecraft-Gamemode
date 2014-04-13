28ea1e0b5008a119023d9894a2b9127eda8ac0e6
Emery Tom <zent.griffith@gmail.com>
2014-04-12 21:44:13 -0700

    Fixed smite.

--- Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/mechanics/bosses/abilities/Smite.java
+++ Legion of Sins/src/main/java/me/waternode/server/legion/of/sins/mechanics/bosses/abilities/Smite.java
@@ -1,5 +1,6 @@
 package me.waternode.server.legion.of.sins.mechanics.bosses.abilities;
 
+import me.waternode.server.legion.of.sins.LOSMain;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -15,7 +16,8 @@ public class Smite extends Abilities {
 
     public Smite(ArrayList<Player> ps) {super(ps, false);}
 
-    public void OnPlayerNear(Player p, Random random) {
+    @Override
+    public void PlayerCast(Player p) {
         World w = p.getWorld();
         Location pLoc = p.getLocation();
         int pX = pLoc.getBlockX();
@@ -23,9 +25,9 @@ public class Smite extends Abilities {
         if (w.getHighestBlockYAt(pX, pZ) - 5 <= pLoc.getY()) {
                 Location loc = p.getLocation();
 
-                loc.setX(p.getLocation().getX() + (random.nextInt(11) - 5));
+                loc.setX(p.getLocation().getX() + (LOSMain.getRandom().nextInt(11) - 5));
                 loc.setY(p.getLocation().getY());
-                loc.setZ(p.getLocation().getZ() + (random.nextInt(11) - 5));
+                loc.setZ(p.getLocation().getZ() + (LOSMain.getRandom().nextInt(11) - 5));
 
                 p.getWorld().strikeLightning(loc);
         }
